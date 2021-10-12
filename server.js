510054a9d0a2f818994eccc65fad449216fee8b3
Maria Victoria <marivibilbaog@gmail.com>
2021-10-12 18:44:40 +0200

    Agrego ejercicios en clase GET y POST

--- server.js
+++ server.js
@@ -11,4 +11,25 @@ app.get("/quotes", function(request, response){
   response.json(quotes);
 });
 
+// /quotes/2
+// /quotes?id=2
+//GET
+app.get("/quotes/:id", function(request, response) {
+  const id = parseInt(request.params.id);
+  // Sacar quote de quotes teniendo el id
+  const quote = quotes.find(q => q.id === id);
+  return response.send(quote);
+});
+
+//POST
+app.post("quotes", function(request, response) {
+  const quote = {
+    author: request.body.author,
+    quote: request.body.quote,
+    id: quotes.length
+  };
+  quotes.push(quote);
+  return response.send(quote);
+});
+
 app.listen(3000, () => console.log("Listening on port 3000"));
